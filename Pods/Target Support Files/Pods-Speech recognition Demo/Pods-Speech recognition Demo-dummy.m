7d4eec9409070998e4e5e8533f8d9ea01239709f
Apps <authentic.identity.ai@gmail.com>
2019-02-20 16:31:37 +0530

    commit

---
+++ Pods/Target Support Files/Pods-Speech recognition Demo/Pods-Speech recognition Demo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Speech_recognition_Demo : NSObject
+@end
+@implementation PodsDummy_Pods_Speech_recognition_Demo
+@end
---
+++ Speech recognition/VoiceOverlay.h
@@ -0,0 +1,19 @@
+//
+//  VoiceOverlay.h
+//  VoiceOverlay
+//
+//  Created by Authentic-Identity-Ai on 25/06/2018.
+//  Copyright © 2018 Authentic-Identity-Ai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VoiceOverlay.
+FOUNDATION_EXPORT double VoiceOverlayVersionNumber;
+
+//! Project version string for VoiceOverlay.
+FOUNDATION_EXPORT const unsigned char VoiceOverlayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VoiceOverlay/PublicHeader.h>
+
+
